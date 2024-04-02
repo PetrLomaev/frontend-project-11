a0dabfe7c4f313c0fbd68eb485bce3980298e26f
Петр Ломаев <petrucho060395@gmail.com>
2024-04-02 22:10:55 +0400

    files step2 bad but work

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import onChange from 'on-change';
 const state = {
   inputValue: '',
 }
-
+/*
 const watchedState = onChange(state, (path) => {
   const input = document.querySelector('#url-input');
   input.addEventListener('input', (e) => {
@@ -12,31 +12,34 @@ const watchedState = onChange(state, (path) => {
     alert(watchedState.inputValue);
   });
 });
-
+*/
 const schema = yup.string().url();
 
 const validate = (fields) => {
   try {
-    schema.isValidSync(fields);
+    schema.validateSync(fields);
     return {};
   } catch (e) {
-    alert('ошибка, неправильный юрл');
+    console.log('ошибка, юрл невалидный');
   }
 };
 
-
-
 const checkForm = () => {
-const buttonAdd = document.querySelector('button[type="submit"]');
-buttonAdd.addEventListener('submit', (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const checkUrl = watchedState.inputValue;
-  alert(checkUrl);
-  validate(checkUrl);
+  //const checkUrl = watchedState.inputValue;
+  const formData = new FormData(e.target);
+  const url = formData.get('url');
+  //validate(url);
+  const pEl = document.querySelector('p[class="feedback m-0 position-absolute small text-danger"]');
+  const inputEl = document.querySelector('input[id="url-input"]');
+  if (!validate(url)) {
+    inputEl.classList.add('is-invalid');
+    pEl.textContent = 'Ссылка должна быть валидным URL';
+  }
 });
 }
-
-
 
 export default checkForm;
 
