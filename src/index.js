0c51b39c000af25f4ab941ae52965a2fecb6045d
Петр Ломаев <petrucho060395@gmail.com>
2024-04-10 22:08:25 +0400

    step4 try3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import 'bootstrap';
 import app from './main.js';
 import axios from 'axios';
 
-const fn = async () => {
-  const response = await axios.get('http://ru.hexlet.io/');
-  console.log(response.data); // тело ответа
+const proxy = (url) => {
+  const root = 'https://allorigins.hexlet.app/get';
+  const rootUrl = new URL(root);
+  //console.log('rootUrl', rootUrl)
+  const inputUrl = encodeURIComponent(url);
+  rootUrl.searchParams.set('url', inputUrl);
+  console.log('rootUrl', rootUrl);
+  return rootUrl;
+};
+
+const fn = async (url) => {
+  const response = await axios.get(proxy(url));
+  console.log('response', response); // тело ответа
+  //const responseJson = JSON.parse(response);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(response, "text/xml");
+  console.log('doc', doc);
 }
-console.log(fn());
+fn('https://lorem-rss.hexlet.app/feed');
 app();
