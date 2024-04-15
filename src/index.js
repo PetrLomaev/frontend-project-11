5679b7d0d9b5de5d748fd00835484a2d722eb551
Петр Ломаев <petrucho060395@gmail.com>
2024-04-15 23:33:16 +0400

    step4 try6

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import './styles.scss';
 import 'bootstrap';
 import app from './main.js';
-import axios from 'axios';
 
-const getProxy = (url) => {
-  const urlProxy = new URL('/get', 'https://allorigins.hexlet.app');
-  urlProxy.searchParams.set('disableCache', 'true');
-  urlProxy.searchParams.set('url', url);
-  console.log('urlProxy.toString', urlProxy.toString());
-  return urlProxy.toString();
-};
-
-const fn = async (url) => {
-  const response = await axios.get(getProxy(url));
-  console.log('response', response); // тело ответа
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(response.data, "text/xml");
-  console.log('doc', doc);
-}
-fn('https://lorem-rss.hexlet.app/feed');
 app();
--- src/main.js
+++ src/main.js
@@ -15,9 +15,23 @@ const validate = (fields) => {
   }
 };
 
+// Функция для прокси
+const getProxy = (url) => {
+  const urlProxy = new URL('/get', 'https://allorigins.hexlet.app');
+  urlProxy.searchParams.set('disableCache', 'true');
+  urlProxy.searchParams.set('url', url);
+  return urlProxy.toString();
+};
+// Функция для получения xml-документа из ссылки. Из него нужно извлекать фиды, посты и т.д.
+const parserToXml = async (url) => {
+  const response = await axios.get(getProxy(url));
+  const parser = new DOMParser();
+  const docXtml = parser.parseFromString(response.data.contents, "text/xml");
+  console.log('docXtml', docXtml);
+  return docXtml;
+}
 
-
-
+// Пока что прокинул сюда функцию для xml. Потом вынести в отдельный файл!
 const checkForm = (paragraph, watchedState, i18nextInstance) => {
   const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
@@ -34,8 +48,34 @@ const checkForm = (paragraph, watchedState, i18nextInstance) => {
       inputEl.classList.remove('in-valid', 'is-invalid');
       paragraph.classList.replace('text-danger', 'text-success');
     }
+    const parsedXml = parserToXml(url);
+    const allItem = Array.from(parsedXml.querySelectorAll('item'));
+    const allItemToObj = allItem.map((item) => {
+      return {
+        title: item.querySelector('title').textContent,
+        description: item.querySelector('description').textContent,
+        link: item.querySelector('link').textContent,
+      }
+    });
+    const divAutoPosts = document.querySelector('div[class="col-md-10 col-lg-8 order-1 mx-auto posts"]');
+    const divPosts = document.createElement('div');
+    divPosts.outerHTML = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div>';
+    const ulPosts = document.createElement('ul');
+    ulPosts.outerHTML = '<ul class="list-group border-0 rounded-0"></ul>';
+    // Ниже дикая жесть, тестирую отрисовку постов фидов и тд
+    allItemToObj.forEach(({ tittle, description, link }) => {
+      const liEl = document.createElement('li');
+      liEl.outerHTML = `<li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+      <a href=${link} class="fw-bold" data-id="" target="_blank" rel="noopener noreferrer">${tittle}</a>
+      <button type="button" class="btn btn-outline-primary btn-sm" data-id="" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>
+      </li>`;
+      ulPosts.append(liEl);
+    })
+    divPosts.append(ulPosts);
+    divAutoPosts.append(divPosts);
   });
 };
+
 const app = async () => {
   const { ru } = resources;
   const pEl = document.querySelector('p[class="feedback m-0 position-absolute small text-danger"]');
