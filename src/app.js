29bfb61f484f113cf554cbb88546365ac33b4614
Петр Ломаев <petrucho060395@gmail.com>
2024-05-07 22:49:08 +0400

    check1 try5

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import axios from 'axios';
 import resources from './locales/index.js';
-import renderFeedAndPosts from './view/view.js';
+import viewFeedAndPosts from './view/view.js';
 import validator from './utils/validator.js';
 import getProxy from './utils/proxify.js';
 import parserToXml from './utils/parser.js';
@@ -46,55 +46,55 @@ const app = () => {
     form, inputEl, buttonAdd, feedbackEl, feedsEl, postsEl, modalTitle, modalBody, modalFooter,
   };
 
-  const viewState = renderFeedAndPosts(state, elements, i18nextInstance);
+  const watchedState = viewFeedAndPosts(state, elements, i18nextInstance);
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    viewState.rssForm.stateForm = 'filling';
+    watchedState.rssForm.stateForm = 'filling';
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    const urlsList = viewState.feeds.map((feed) => feed.url);
+    const urlsList = watchedState.feeds.map((feed) => feed.url);
     validator(url, urlsList, i18nextInstance)
       .then((validUrl) => {
-        viewState.rssForm.error = null;
-        viewState.rssForm.stateForm = 'processing';
+        watchedState.rssForm.error = null;
+        watchedState.rssForm.stateForm = 'processing';
         return axios.get(getProxy(validUrl));
       })
       .then((response) => {
         const [feed, posts] = parserToXml(response.data.contents);
         const newFeed = { ...feed, url };
         const newPosts = posts.map((post) => ({ ...post, feedId: newFeed.id }));
-        viewState.feeds = [newFeed, ...viewState.feeds];
-        viewState.posts = [...newPosts, ...viewState.posts];
-        viewState.rssForm.stateForm = 'sucess';
+        watchedState.feeds = [newFeed, ...watchedState.feeds];
+        watchedState.posts = [...newPosts, ...watchedState.posts];
+        watchedState.rssForm.stateForm = 'sucess';
       })
       .catch((err) => {
-        viewState.rssForm.inputValueStatus = false;
+        watchedState.rssForm.inputValueStatus = false;
         if (err.name === 'ValidationError') {
-          viewState.rssForm.error = err.message;
+          watchedState.rssForm.error = err.message;
         } else if (err.isParseError) {
-          viewState.rssForm.error = 'form.errors.notContainValidRss';
+          watchedState.rssForm.error = 'form.errors.notContainValidRss';
         } else if (axios.isAxiosError(err)) {
-          viewState.rssForm.error = 'form.errors.networkError';
+          watchedState.rssForm.error = 'form.errors.networkError';
         } else {
-          viewState.rssForm.error = err;
+          watchedState.rssForm.error = err;
         }
-        viewState.rssForm.stateForm = 'filling';
+        watchedState.rssForm.stateForm = 'filling';
       });
 
     postsEl.addEventListener('click', (event) => {
       if (event.target.closest('a')) {
         const { id } = event.target.dataset;
-        viewState.uiState.visitedLinks.add(id);
+        watchedState.uiState.visitedLinks.add(id);
       }
       if (event.target.closest('button')) {
         const { id } = event.target.dataset;
-        viewState.uiState.visitedLinks.add(id);
-        viewState.uiState.modalId = id;
+        watchedState.uiState.visitedLinks.add(id);
+        watchedState.uiState.modalId = id;
       }
     });
 
-    setTimeout(() => updatePosts(viewState), 5000);
+    setTimeout(() => updatePosts(watchedState), 5000);
   });
 };
 
